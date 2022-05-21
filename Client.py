6abac6be00759f7689674e24c2440fde8b20ab65
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-05-21 04:15:25 +0200

    test send cipher and receive fail no.1
    it was enabled to send and receive but the message encrypted is broken

--- Client.py
+++ Client.py
@@ -1,18 +1,56 @@
 import socket
 import RSA
-
+from Crypto.Util import number
+from math import log2
+import os
+n_length = 8
 s = socket.socket()
 s.connect(('127.0.0.1',12345))
 
-def decrypt(cipher_text):
+
+def Auto_key_generation():
+    p = number.getPrime(n_length//2) # 8 bytes * 8 = 64 bits (1 byte = 8 bits)
+    q = number.getPrime(n_length//2) # 8 bytes * 8 = 64 bits (1 byte = 8 bits)
+    while p == q :
+        q = number.getPrime(n_length//2) # 8 bytes * 8 = 64 bits (1 byte = 8 bits)
+     
+    n = p * q
+    phi_n = (p-1)*(q-1)
+    e = number.getPrime(int(log2(phi_n)))
+    while(RSA.GCD(phi_n, e) != 1):
+        e = number.getPrime(int(log2(phi_n)))
+    d = RSA.InvertModulo(e, phi_n)
+    return e,d,n,p,q
+
+
+def Manual_key_generation(string):
     
-    plain_text = cipher_text
+    p,q,e = string.split(",")
+    n = p * q
+    phi_n = (p-1)*(q-1)
+    d = RSA.InvertModulo(e, phi_n)
+    return e,d,n,p,q
+
+def decrypt(cipher_text,p,q,e):
     
+    plain_text = RSA.Decrypt(cipher_text, p, q, e)
+    print(plain_text,cipher_text, p, q, e )
     return plain_text
 
+#start sendingg
 
+Auto_Or_Manual = input("Do you want key generation to be automatic or manual ? 1 for auto , 2 for manual    ")
+if int(Auto_Or_Manual )== 1 :
+    e,d,n,p,q = Auto_key_generation()
+elif Auto_Or_Manual == 2 :
+    e,d,n,p,q = Manual_key_generation(input("Please enter p,q,e saperated by comma"))
+s.send(RSA.ConvertToStr(n).encode())
+s.send(RSA.ConvertToStr(e).encode())
+print(n,e)
 while True:
     cipher = s.recv(1024)
-    plain_text = decrypt(cipher.decode())
+    plain_text = decrypt(cipher.decode(),p,q,e)
     print ("sender:",plain_text)
+    
+
 s.close()
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket
 import RSA
-
 print("trying to connect")
 s = socket.socket()
 port = 12345
@@ -10,14 +9,21 @@ c, addr = s.accept()
 print("connected successfully")
 print ("Socket Up and running with a connection from",addr)
 
-def encrypt(plain_text):
-    cipher_text = plain_text
+def encrypt(plain_text,n,e):
+    cipher_text = RSA.Encrypt(plain_text, n, e)
+    print(plain_text,cipher_text,n,e)
     return cipher_text
+
+
+n_str = c.recv(1024)
+n = RSA.ConvertToInt(n_str.decode())
+e_str = c.recv(1024)
+e = RSA.ConvertToInt(e_str.decode())
 
 
 while True:
     plain_text = input("sender: ")
-    cipher_text = encrypt(plain_text)
+    cipher_text = encrypt(plain_text,n,e)
     c.send(cipher_text.encode())
     
 c.close()
